a82c5c0761c9d9017d1b9fa78a4ce0d9d623ce5d
Samu Landen <samu.landen@gmail.com>
2022-09-16 11:02:26 +0300

    added all fucntions to database.js and also made rest work for everything in app.js

--- app.js
+++ app.js
@@ -2,17 +2,71 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 8080;
-const { readAllTableware, createTableware } = require("./database");
+const {
+  readAllTableware,
+  createTableware,
+  readTableware,
+  updateTableware,
+  deleteTableware,
+  readAllOffice,
+  readOffice,
+  createOffice,
+  updateOffice,
+  deleteOffice,
+} = require("./database");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/tableware", async (req, res) => {
-  res.json(await readAllTableware());
+app.get("/office/:office_id/tableware", async (req, res) => {
+  const office_id = req.params.office_id;
+  res.json(await readAllTableware(office_id));
 });
 
-app.post("/tableware", async (req, res) => {
-  res.json(await createTableware(req.body));
+app.get("/office", async (req, res) => {
+  res.json(await readAllOffice());
+});
+
+app.get("/office/:office_id/tableware/:id", async (req, res) => {
+  const office_id = req.params.office_id;
+  const id = req.params.id;
+  res.json(await readTableware(office_id, id));
+});
+
+app.get("/office/:id", async (req, res) => {
+  const id = req.params.id;
+  res.json(await readOffice(id));
+});
+
+app.post("/office/:office_id/tableware", async (req, res) => {
+  const office_id = req.params.office_id;
+  res.json(await createTableware(office_id, req.body));
+});
+
+app.post("/office", async (req, res) => {
+  res.json(await createOffice(req.body));
+});
+
+app.put("/office/:id", async (req, res) => {
+  const id = req.params.id;
+  res.json(await updateOffice(id, req.body));
+});
+
+app.put("/office/:office_id/tableware/:id", async (req, res) => {
+  const office_id = req.params.office_id;
+  const id = req.params.id;
+  res.json(await updateTableware(office_id, id, req.body));
+});
+
+app.delete("/office/:id", async (req, res) => {
+  const id = req.params.id;
+  res.json(await deleteOffice(id));
+});
+
+app.delete("/office/:office_id/tableware/:id", async (req, res) => {
+  const office_id = req.params.office_id;
+  const id = req.params.id;
+  res.json(await deleteOffice(office_id, id));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const { Pool } = require("pg");
 const pool = new Pool();
 
-async function readAllTableware() {
+async function readAllTableware(office_id) {
   try {
-    const res = await pool.query("SELECT name, qty FROM Tableware");
+    const res = await pool.query(
+      "SELECT tableware.name, tableware.qty, office.location FROM tableware INNER JOIN office ON office.id = tableware.office_id WHERE office_id = $1",
+      [office_id]
+    );
     console.log(res.rows);
     return res.rows;
   } catch (err) {
@@ -11,13 +14,102 @@ async function readAllTableware() {
   }
 }
 
-async function createTableware(tableware) {
+async function readTableware(office_id, id) {
   try {
     const res = await pool.query(
-      "INSERT INTO Tableware (name, qty) VALUES ($1, $2)",
-      [tableware.name, tableware.qty]
+      "SELECT id, name, qty FROM tableware WHERE office_id = $1 AND id = $2;",
+      [office_id, id]
+    );
+    return res.rows[0];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function createTableware(office_id, tableware) {
+  try {
+    const res = await pool.query(
+      "INSERT INTO tableware (name, qty, office_id) VALUES ($1, $2, $3)",
+      [tableware.name, tableware.qty, office_id]
     );
     return;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function updateTableware(office_id, id, tableware) {
+  try {
+    const res = await pool.query(
+      "UPDATE tableware SET name = $3, qty = $4, office_id = $5 WHERE office_id = $1 AND id = $2",
+      [office_id, id, tableware.name, tableware.qty, tableware.office_id]
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function deleteTableware(office_id, id) {
+  try {
+    const res = await pool.query(
+      "DELETE FROM tableware WHERE office_id = $1 AND id = $2",
+      [office_id, id]
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function readAllOffice() {
+  try {
+    const res = await pool.query(
+      "SELECT name, location, starting_year FROM office"
+    );
+    console.log(res.rows);
+    return res.rows;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function readOffice(id) {
+  try {
+    const res = await pool.query(
+      "SELECT id, name, location, starting_year FROM office WHERE id = $1;",
+      [id]
+    );
+    return res.rows[0];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function createOffice(office) {
+  try {
+    const res = await pool.query(
+      "INSERT INTO office (name, location, starting_year) VALUES ($1, $2, $3)",
+      [office.name, office.location, office.starting_year]
+    );
+    return;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function updateOffice(id, office) {
+  try {
+    const res = await pool.query(
+      "UPDATE office SET name = $2, location = $3, starting_year = $4 WHERE id = $1",
+      [id, office.name, office.location, office.starting_year]
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function deleteOffice(id) {
+  try {
+    const res = await pool.query("DELETE FROM office WHERE id = $1", [id]);
   } catch (err) {
     console.log(err);
   }
@@ -26,4 +118,12 @@ async function createTableware(tableware) {
 module.exports = {
   readAllTableware,
   createTableware,
+  readTableware,
+  updateTableware,
+  deleteTableware,
+  readAllOffice,
+  readOffice,
+  createOffice,
+  updateOffice,
+  deleteOffice,
 };
